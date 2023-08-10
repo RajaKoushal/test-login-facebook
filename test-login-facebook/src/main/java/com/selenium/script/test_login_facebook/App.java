c2e0d0f67038d54cf9d124ce63d53082af0957c8
Kaval <Kaval@LAPTOP-5AVVF2U5>
2023-08-10 20:33:03 +0530

    First Commit

---
+++ test-login-facebook/src/main/java/com/selenium/script/test_login_facebook/App.java
@@ -0,0 +1,13 @@
+package com.selenium.script.test_login_facebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test-login-facebook/src/test/java/com/selenium/script/test_login_facebook/AppTest.java
@@ -0,0 +1,20 @@
+package com.selenium.script.test_login_facebook;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
